799ce5e24540f04495b2bb7ee83f40f93094dec2
Wayne Chipchase <wayne.chipchase@gmail.com>
2019-04-08 21:25:51 -0600

    wip

---
+++ javascripts/helpers/book.js
@@ -0,0 +1,11 @@
+const bookInfo = {
+    price: 24.99,
+    title: 'Fifty Shades of Chicken',
+    image: './assets/book.jpg'
+};
+
+const getBook = () => {
+    return bookInfo;
+};
+
+export default { getBook };
